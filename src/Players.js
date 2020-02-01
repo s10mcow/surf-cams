231214298a4673c986d56539c04f65b72e970d82
Sten Muchow <sten.muchow@gmail.com>
2020-02-01 16:31:05 +0000

    add usr stuff

--- src/Players.js
+++ src/Players.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import Drawer from '@material-ui/core/Drawer';
-import Layout from './components/Layout';
+
+import styled from 'styled-components';
 import PlayersContainer from './containers/PlayersContainer';
+import { Menu } from '@material-ui/icons';
+import { IconButton } from '@material-ui/core';
+
+import AppBar from './components/AppBar';
+const PageHeader = styled.header`
+    display: flex;
+    justify-content: space-between;
+    flex: 1;
+    align-items: center;
+    max-height: 45px;
+    width: 100%;
+    background-color: #777777;
+    color: white;
+    padding: 10px;
+    h1 {
+        margin-left: 30px;
+        font-size: 18px;
+    }
+    button {
+        margin-left: auto;
+        margin-right: 10px;
+    }
+`;
 
 export default function App() {
-    const [isOpen, toggleOpen] = React.useState(false);
-
     return (
         <>
-            <Drawer open={isOpen} onClose={() => toggleOpen(!isOpen)}>
-                change country
-            </Drawer>
-            <Layout>
-                <header className="page__header">
-                    <h1>howisthe.surf</h1>
-                </header>
-
-                <PlayersContainer />
-            </Layout>
+            <AppBar />
+            <PlayersContainer />
         </>
     );
 }
--- src/components/Link.js
+++ src/components/Link.js
@@ -1,27 +1,34 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Link = ({ active, children, onClick }) => {
     if (active) {
-        return <li className="link link--active"><a>{children}</a></li>
+        return (
+            <li className="link link--active">
+                <a>{children}</a>
+            </li>
+        );
     }
 
     return (
         <li className="link">
-            <a href="#" onClick={e => {
-                e.preventDefault();
-                onClick();
-            }}>
+            <a
+                href="#"
+                onClick={e => {
+                    e.preventDefault();
+                    onClick();
+                }}
+            >
                 {children}
             </a>
         </li>
-    )
+    );
 };
 
 Link.propTypes = {
     active: PropTypes.bool.isRequired,
     children: PropTypes.node.isRequired,
-    onClick: PropTypes.func.isRequired
+    onClick: PropTypes.func.isRequired,
 };
 
-export default Link
+export default Link;
