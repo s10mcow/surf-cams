612ffd45310dd1278f867ff98c79a2cb8160b367
Sten Muchow <sten.muchow@gmail.com>
2020-11-20 11:09:19 -0600

    wip

--- src/store/app/app.sagas.js
+++ src/store/app/app.sagas.js
@@ -1,10 +1,10 @@
-import { takeLatest, call, put, fork } from 'redux-saga/effects';
+import { takeLatest, put } from 'redux-saga/effects';
 import TYPES from './app.types';
-import userActions from '../user/user.actions';
+// import userActions from '../user/user.actions';
 import actions from './app.actions';
-import api from '../api';
-import { watchOnLogin } from '../user/user.saga';
-import netlifyIdentity from 'netlify-identity-widget';
+// import api from '../api';
+// import { watchOnLogin } from '../user/user.saga';
+// import netlifyIdentity from 'netlify-identity-widget';
 
 function* initApp() {
     try {
